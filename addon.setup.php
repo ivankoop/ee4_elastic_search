8b87d88d7adbdfb6f6444f719149f75c04f95605
Ivan Koop <ivankoop3@gmail.com>
2018-09-10 15:30:25 -0400

    fix in setup

--- addon.setup.php
+++ addon.setup.php
@@ -4,7 +4,7 @@ return array(
     'author'      => 'Ivan Koop',
     'author_url'  => 'https://vikm.co/',
     'name'        => 'Json Search',
-    'description' => 'json output of channel entries search',
+    'description' => 'json output of channel entries by search',
     'version'     => '1.0.0',
     'namespace'   => '\\',
 );
