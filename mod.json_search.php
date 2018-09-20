30efafc6292d8600cbc72d521b7deca2373665cc
Ivan Koop <ivankoop3@gmail.com>
2018-09-20 11:44:34 -0400

    ..

--- mod.json_search.php
+++ mod.json_search.php
@@ -67,11 +67,10 @@ class Json_search
             'body' => [
                 'query' => [
                     'multi_match' => [
-                        'fields' => ['title^7','description^2'],
-                        'type' => 'cross_fields',
-
+                        'fields' => ['title^10','description'],
+                        'type' => 'most_fields',
                         'query' => $this->like_input,
-                    
+                        'fuzziness' => "1"
                     ]
                 ]
             ]
